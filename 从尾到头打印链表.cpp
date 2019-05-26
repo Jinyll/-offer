2f6ae62728d39f7e89ce2918e9365f34581e0be1
Jinyll <35912845+Jinyll@users.noreply.github.com>
2019-05-26 11:06:55 +0800

    Update 从尾到头打印链表.cpp

--- 从尾到头打印链表.cpp
+++ 从尾到头打印链表.cpp
@@ -1,5 +1,14 @@
 /*输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。*/
 //从尾到头，用栈
+/**
+*  struct ListNode {
+*        int val;
+*        struct ListNode *next;
+*        ListNode(int x) :
+*              val(x), next(NULL) {
+*        }
+*  };
+*/
 class Solution {
 public:
     vector<int> printListFromTailToHead(ListNode* head) {
