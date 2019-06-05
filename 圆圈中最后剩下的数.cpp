f573b89064ae6d7e103879692f09035a7cc607a9
Jinyll <35912845+Jinyll@users.noreply.github.com>
2019-06-05 14:52:31 +0800

    Update 圆圈中最后剩下的数.cpp

--- 圆圈中最后剩下的数.cpp
+++ 圆圈中最后剩下的数.cpp
@@ -30,7 +30,7 @@ public:
     {
         if(n<=0 || m<=0) return -1;
         int last=0;
-        for(int i=0;i<=n;i++) last=(last+m)%i;
+        for(int i=0;i<=n;i++) last=(last+m)%2;
         return last;
     }
 };
